9d6567ad3e7bd85d2f9cb9f389e58f210222faf2
Sina Khanifar <sina.khanifar@gmail.com>
2013-11-21 15:46:45 -0800

    Added leaderboard and getting coinbase/stripe working

--- static/js/paymentform.js
+++ static/js/paymentform.js
@@ -1,6 +1,7 @@
 
 ////////////////
 // Custom Amount & Reward level
+////////////////
 
 var amountInput = document.getElementById("payform").amount;
 function highlight_choice(target){
@@ -35,3 +36,58 @@ function onAmountChange(){
 }
 amountInput.addEventListener("change",onAmountChange);
 
+////////////////
+// Swinging Click
+////////////////
+
+(function(exports){
+
+	function getPos(el) {
+	    for (var lx=0, ly=0;
+	         el != null;
+	         lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+	    return {x:lx, y:ly};
+	}
+	
+	var _locked = false;
+	exports.swingTo = function(anchor){
+
+		// One swing at a time
+		if(_locked) return;
+		_locked=true;
+
+		// Find scroll Y pos
+		var anchorDOM = document.querySelector(anchor);
+		var anchorPos = getPos(anchorDOM);
+		var currY = window.scrollY;
+		var gotoY = anchorPos.y;
+
+		// Ease into it
+		var t = 0;
+		var interval = setInterval(function(){
+
+			var y = currY + ( (1-Math.cos(t))/2 )*(gotoY-currY);
+			window.scrollTo(0,y);
+
+			t += Math.PI/20;
+			if(t>Math.PI){
+				clearInterval(interval);
+				_locked = false;
+			}
+
+		},30);
+		
+	};
+
+})(window);
+
+
+////////////////
+// Leaderboard
+////////////////
+
+var leaderboard = document.getElementById("leaderboard");
+document.getElementById("show_all_donators").onclick = function(){
+	leaderboard.removeAttribute("collapsed");
+	leaderboard.setAttribute("expanded","true");
+};
